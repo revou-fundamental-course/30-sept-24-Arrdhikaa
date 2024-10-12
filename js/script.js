cbc7087a2dfa73963d2d7d1fc2b56fc0d98db89e
Arrdhikaa <muhammadardhika307@gmail.com>
2024-10-12 11:05:36 +0700

    version 4

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-console.log("hello world");
 
 function validateform() {
-    console.log("berhasil terpanggil");
     const inputsuhu = document.getElementById("input-suhu").value;
-    
+
     if (inputsuhu == '') {
         alert("input anda kosong");
     } else{
@@ -15,13 +13,14 @@ function calculate(value) {
     let result = parseInt(value) * 9/5 + 32;
     document.getElementById("hasil-suhu").value = result;
     document.getElementById("how-to-suhu").value = value + '* 9/5 + 32 = '+ result ;
- 
 }
 
 function reset(){
     document.getElementById("input-suhu").value = "";
     document.getElementById("hasil-suhu").value = "";
     document.getElementById("how-to-suhu").value = "";
+}
 
-
+function reverse() {
+    
 }
